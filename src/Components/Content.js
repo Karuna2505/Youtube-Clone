f74f567be46d6f1b9e4ef247ff4590c9119266f5
Karuna2505 <karunamathur14@gmail.com>
2024-04-17 11:38:44 +0530

    Swiper in Content

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import {Navigation} from "swiper/modules";
+import {Swiper,SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 function Content({update}) {
   const categories = [
     'All', 'Music', 'Lo-Fi', 'Web Development', 'Computer Science', 'Playlists',
     'Lives', 'Soldering', 'B Praak', 'Electronics', 'Indian pop music',
-    'Home improvement', 'Dramedy', 'Physics', 'News', 'Comedy','Fashion','Gaming','Job Interviews'
+    'Home improvement', 'Dramedy', 'Physics', 'News', 'Comedy','Fashion','Gaming','Job Interviews','Pranks'
   ];
   const [selectedCategory,setSelectedCategory]=useState("All");
   function handleClick(category){
@@ -17,18 +21,18 @@ function Content({update}) {
     }
   }
   return (
-    <div className='h-12 w-full flex flex-wrap fixed mx-12 my-8 z-40 bg-black items-center'>
-    {categories.map((category, index) => (
-      <button key={index} onClick={() => handleClick(category)}>
-      <div
-        key={index}
-        className={`${selectedCategory === category ? 'bg-white text-black' : 'bg-[rgb(255,255,255,0.16)] text-white'} rounded-md px-2.5 mx-1 font-medium text-xs flex justify-center items-center h-6 hover:bg-[rgb(255,255,255,0.3)]`}
-      >
-        {category}
-      </div>
-      </button>
+  <Swiper
+  modules={[Navigation]}
+  spaceBetween={20}
+  slidesPerView={19}
+  navigation
+  className='!fixed !pt-3 h-12 !my-8 !mx-12 !w-full z-40 bg-black items-center'
+  >
+    {categories.map((category,index) => (
+      <SwiperSlide key={index} onClick={() => handleClick(category)} className={`${selectedCategory === category ? 'bg-white text-black' : 'bg-[rgb(255,255,255,0.16)] text-white'} rounded-md !px-2.5 !mx-1 !font-medium !text-xs !flex !justify-center !items-center !h-6 hover:bg-[rgb(255,255,255,0.3)] !w-max`}>{category}</SwiperSlide>
     ))}
-  </div>
+    
+  </Swiper>
   );
 }
 
